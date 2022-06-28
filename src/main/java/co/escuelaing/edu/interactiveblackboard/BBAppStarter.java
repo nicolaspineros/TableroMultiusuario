cb6eab37c12056e49748483d37859bb459f47e57
unknown <oscar.pineros-c@labinfo.is.escuelaing.edu.co>
2022-06-28 10:32:44 -0500

    Tablero

---
+++ src/main/java/co/escuelaing/edu/interactiveblackboard/BBAppStarter.java
@@ -0,0 +1,10 @@
+package co.escuelaing.edu.interactiveblackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BBAppStarter {
+    public static void main(String[] args){
+        SpringApplication.run(BBAppStarter.class,args);
+    }
+}
